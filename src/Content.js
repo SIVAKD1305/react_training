006cc7947aace32afa35a8a132935428a63ddbe5
SIVAKD1305 <133256562+SIVAKD1305@users.noreply.github.com>
2024-11-04 19:06:28 +0530

    modified:   src/Content.js

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,26 @@
 import React from 'react'
-
+import { useState } from 'react';
 const Content = () => {
     const change = () =>{
         const greetings = ["Hey", "Hi", "Hello", "Welcome"];
         const n = Math.floor(Math.random()*4);
         return greetings[n];
      }
+     const [count, setCount] = useState(0);
+     const increment = () => {
+      setCount(inc => inc+1);
+     }
+     const decrement = () => {
+      setCount(dec => dec-1);
+     }
   return (
+    <>
+    <button onClick={increment}>+</button>
     <p>
-      Content {change()}
+      {count}
     </p>
+    <button onClick={decrement}>-</button>
+    </>
   )
 }
 
